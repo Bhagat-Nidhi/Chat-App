f8bd14590b8c32594c060b1a0d6feb10f2a1f736
Bhagat-Nidhi <emailbhagatnidhi@gmail.com>
2025-04-11 10:51:44 +0530

    resolved erroe

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,6 @@
-import { Routes, Route } from "react-router-dom";
+//import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
 
 import HomePage from "./pages/HomePage";
